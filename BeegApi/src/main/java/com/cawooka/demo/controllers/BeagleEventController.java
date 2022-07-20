3c62ad0d34cab341a858a17977b2f353afb16110
Nick Peachey <nick.peachey@gmail.com>
2022-07-20 16:54:11 +0100

    added avro beagle file

--- BeegApi/src/main/java/com/cawooka/demo/controllers/BeagleEventController.java
+++ BeegApi/src/main/java/com/cawooka/demo/controllers/BeagleEventController.java
@@ -1,6 +1,5 @@
 package com.cawooka.demo.controllers;
 
-import concrete.Beagle;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +12,7 @@ public class BeagleEventController {
 
     @GetMapping("/put")
     public ResponseEntity<String> put() {
-        Beagle beagle = new Beagle("Willow");
 
-        return new ResponseEntity<>(beagle.getName(), HttpStatus.OK);
+        return new ResponseEntity<>("hello willow", HttpStatus.OK);
     }
 }
---
+++ Services/src/main/java/producers/kafka/BeagleEventProducer.java
@@ -0,0 +1,6 @@
+package producers.kafka;
+
+
+public class BeagleEventProducer {
+
+}
